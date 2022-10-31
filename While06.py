a10212e6ab5d866cee1ed7922140e214775b3aa0
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-31 08:51:32 +0500

    While06.py

--- While06.py
+++ While06.py
@@ -7,3 +7,21 @@ def main(s):
     Returns:
         int: return answer
     """
+    k=0
+    i=0
+    while i<len(s):
+        if str(s[i])==('a'):
+            k+=1
+        if str(s[i])==('e'):
+            k+=1
+        if str(s[i])==('o'):
+            k+=1
+        if str(s[i])==('i'):
+            k+=1
+        if str(s[i])==('u'):
+            k+=1
+        i+=1
+    
+    return k
+
+print(main("CodeschoolUz"))
