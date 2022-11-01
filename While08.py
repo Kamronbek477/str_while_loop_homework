17fa3262f2a4531127d4eb3235b9c8c956b95f99
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-11-01 15:44:34 +0500

    While08.py

--- While08.py
+++ While08.py
@@ -6,4 +6,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    k=0 
+    i=0
+    while i<len(s):
+        if str(s[i]).isdigit():
+            if int(s[i])%2==1:
+                k+=1
+            else:
+                k+=0
+        i+=1
+    return k
+print(main("135246"))
